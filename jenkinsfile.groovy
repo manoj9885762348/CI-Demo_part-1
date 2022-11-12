1c057392d12e38a028ee997383fb272057703d24
manoj <manojkumar9542@gmail.com>
2022-11-12 16:58:41 +0530

    modified

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,10 +4,11 @@ Pipeline {
 
     stages{
 
-        stage{'Git Checkout'}
+        stage('Git Checkout'){
 
            steps{
-            git branch: 'main', url: 'https://github.com/manoj9885762348/CI-Demo_part-1.git'
-           }
+                git branch: 'main', url: 'https://github.com/manoj9885762348/CI-Demo_part-1.git'
+            }
+        }    
     }
 }
