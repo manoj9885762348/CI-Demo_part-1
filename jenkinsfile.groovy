74f7f27c5047951f002e9d57069accfbcacb16c8
manoj <manojkumar9542@gmail.com>
2022-11-12 16:18:03 +0530

    git Checkout

---
+++ jenkinsfile.groovy
@@ -0,0 +1,13 @@
+Pipeline {
+
+    agent any
+
+    stages{
+
+        stage{'Git Checkout'}
+
+           steps{
+            git branch: 'main', url: 'https://github.com/manoj9885762348/CI-Demo_part-1.git'
+           }
+    }
+}
